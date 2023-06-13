8796826d23710c4816da706d45ee0af9906eae35
inde153 <inde153@naver.com>
2023-06-14 03:22:10 +0900

    Learn : apply DI

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantResolver } from './restaurants.resolver';
+import { RestaurantService } from './restaurants.service';
 
 @Module({
-  providers: [RestaurantResolver],
+  // inject(주입을 위한 import)
+  imports: [TypeOrmModule.forFeature([Restaurant])],
+  providers: [RestaurantResolver, RestaurantService],
 })
 export class RestaurantsModule {}
