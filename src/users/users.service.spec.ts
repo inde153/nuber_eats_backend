bd998c08e246bf1deb63ae972759380df1a8b011
inde153 <inde153@naver.com>
2023-07-06 02:17:32 +0900

    Learn : createAccount unit test (user exists)

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -59,8 +59,23 @@ describe('UserService', () => {
   });
 
   describe('createAccount', () => {
-    it('should fail if user exists', () => {});
+    it('should fail if user exists', async () => {
+      usersRepository.findOne.mockResolvedValue({
+        id: 1,
+        email: 'nsandasnsa@naver.com',
+      });
+      const result = await service.createAccount({
+        email: '',
+        password: '',
+        role: 0,
+      });
+      expect(result).toMatchObject({
+        ok: false,
+        error: 'There is a user with that email already',
+      });
+    });
   });
+
   it.todo('createAccount');
   it.todo('login');
   it.todo('findById');
