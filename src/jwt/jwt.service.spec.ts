57bc14196ed3a7ef85aea5e0302c35337022fb7f
inde153 <inde153@naver.com>
2023-08-07 03:05:13 +0900

    Add : jwt Sign Test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,14 @@
 import { Test } from '@nestjs/testing';
+import { string } from 'joi';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 
@@ -22,6 +30,16 @@ describe('JwtService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  it.todo('sign');
-  it.todo('verify');
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = 1;
+      const token = service.sign({ id: ID });
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenLastCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
+  describe('verify', () => {
+    it('should return the decoded token', () => {});
+  });
 });
