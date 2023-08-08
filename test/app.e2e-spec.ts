21adbf6b79e137fb906ed62c020f98fa1fb1036c
inde153 <inde153@naver.com>
2023-08-09 02:35:46 +0900

    Revert "Learn : e2e test start"
    This reverts commit 3beaa91216428acd02f4884f020bbdf18055125b.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,17 +3,22 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('UserModule (e2e)', () => {
+describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = module.createNestApplication();
+    app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it.todo('me');
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
 });
