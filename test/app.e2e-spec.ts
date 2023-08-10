5bd6f370e0a90b578e5a83584af6c558fd0595bb
inde153 <inde153@naver.com>
2023-08-11 05:03:18 +0900

    Add : e2e setting

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { DataSource } from 'typeorm';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -13,6 +14,23 @@ describe('UserModule (e2e)', () => {
 
     app = module.createNestApplication();
     await app.init();
+  });
+
+  //Jest did not exit one second after the test run has completed.에러 해결
+  afterAll(async () => {
+    // TypeORM 0.3.0 부터 Connection이 deprecated되고 DataSource 써야됨
+    const dataSource: DataSource = new DataSource({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+    const connection: DataSource = await dataSource.initialize();
+    await connection.dropDatabase(); // 데이터베이스 삭제
+    await connection.destroy(); // 연결 해제
+    await app.close();
   });
 
   it.todo('me');
